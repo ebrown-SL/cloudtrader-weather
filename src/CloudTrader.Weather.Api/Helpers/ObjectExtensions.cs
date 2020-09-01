98f3a590bfdb18f2c86060e9fdf5cbd627cf590f
Laura Warren <lwarren@scottlogic.com>
2020-09-01 15:04:19 +0100

    Add extension method for foreach with index

--- src/CloudTrader.Weather.Api/Helpers/ObjectExtensions.cs
+++ src/CloudTrader.Weather.Api/Helpers/ObjectExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace CloudTrader.Weather
@@ -17,6 +19,12 @@ namespace CloudTrader.Weather
                 Encoding.UTF8,
                 "application/json"
             );
+        }
+
+        public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
+        {
+            var i = 0;
+            foreach (var e in ie) action(e, i++);
         }
     }
 }
