44bccdf379a97708e5d9dbfe556a2adcd88d746b
Laura Warren <lwarren@scottlogic.com>
2020-09-02 09:57:42 +0100

    Add Swagger and tidy code (remove commented lines, etc.)

--- src/CloudTrader.Weather.Api/Controllers/ExternalWeatherController.cs
+++ src/CloudTrader.Weather.Api/Controllers/ExternalWeatherController.cs
@@ -1,5 +1,8 @@
 ﻿using CloudTrader.Weather.Api.Interfaces;
+using CloudTrader.Weather.Api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +22,10 @@ namespace CloudTrader.Weather.Api.Controllers
         }
 
         [HttpGet("{city}/current")]
-        /*[SwaggerOperation(
-            Summary = "Get all traders",
-            Description = "Returns an object containing an array of all traders")]
-        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(GetAllTradersResponseModel))]*/
-
+        [SwaggerOperation(
+            Summary = "Get weather for city",
+            Description = "Returns an object containing the relevant weather data for a specific city")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(WeatherDatum))]
         public async Task<IActionResult> GetWeatherForCity(string city)
         {
             var weather = await _externalWeatherService.GetExternalWeather(city);
@@ -32,11 +34,10 @@ namespace CloudTrader.Weather.Api.Controllers
         }
 
         [HttpGet("all/current")]
-        /*[SwaggerOperation(
-            Summary = "Get all traders",
-            Description = "Returns an object containing an array of all traders")]
-        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(GetAllTradersResponseModel))]*/
-
+        [SwaggerOperation(
+            Summary = "Get weather for all cities",
+            Description = "Returns an object with a key-value pair for each city: key = city name; value = weather data object for city")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(Dictionary<string, WeatherDatum>))]
         public async Task<IActionResult> GetWeatherForAll()
         {
             var weather = await _externalWeatherService.GetExternalWeatherForAll();
--- src/CloudTrader.Weather.Api/Startup.cs
+++ src/CloudTrader.Weather.Api/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CloudTrader.Weather.Api
 {
@@ -28,33 +29,30 @@ namespace CloudTrader.Weather.Api
         {
             services.AddControllers(x => x.AllowEmptyInputInBodyModelBinding = true);
             services.AddScoped<IExternalWeatherService, ExternalWeatherService>();
-            //services.AddScoped<ITraderRepository, TraderRepository>();
-            //services.AddAutoMapper(typeof(TraderProfile));
             services.AddMvc();
-            //services.AddDbContext<TraderContext>();
-            /*services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "CloudTrader-Traders API",
-                    Description = "Endpoints for the CloudTrader-Traders service"
+                    Title = "CloudTrader-Weather API",
+                    Description = "Endpoints for the CloudTrader-Weather service"
                 });
 
                 c.EnableAnnotations();
-            });*/
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseExceptionHandler("/error");
 
-            //app.UseSwagger();
+            app.UseSwagger();
 
-           /* app.UseSwaggerUI(c =>
+            app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloudTrader-Traders API");
-            });*/
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloudTrader-Weather API");
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
