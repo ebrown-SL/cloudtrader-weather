ffee56ba6782b6ade3e20060e06aa4b570f877e3
Laura Warren <lwarren@scottlogic.com>
2020-11-27 14:52:36 +0000

    Add Azure timed function, and functionality to get new weather data from weather service

---
+++ AzureFunctionUpdateWeather/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using CloudTrader.Weather.Api.Models;
+
+namespace AzureFunctionUpdateWeather
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task RunAsync([TimerTrigger("0 0 8 * * * ")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var g = new GetAllWeather();
+            Console.WriteLine(await g.GetAllWeatherData());
+        }
+    }
+
+    static class HttpResponseMessageExtensions
+    {
+        public static async Task<T> ReadAsJson<T>(this HttpResponseMessage message)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(
+                await message.Content.ReadAsStringAsync()
+            );
+        }
+    }
+}
---
+++ AzureFunctionUpdateWeather/GetAllWeather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using CloudTrader.Weather.Api.Models;
+
+namespace AzureFunctionUpdateWeather
+{
+    public class GetAllWeather
+    {
+        public async Task<Dictionary<string, WeatherDatum>> GetAllWeatherData()
+        {
+            using var client = new HttpClient();
+
+            // var uri = "https://cloudtrader.ukwest.cloudapp.azure.com/externalweather/all/current"
+            var uri = "http://localhost:5888/externalweather/all/current";
+
+            var response = await client.GetAsync(uri);
+
+            return await response.ReadAsJson<Dictionary<string, WeatherDatum>>();
+        }
+    }
+}
